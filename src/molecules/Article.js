3366a419069ca2fe0d5e8a13761288f8cae6e5a0
Peter Phan <peterphan0422@gmail.com>
2021-08-18 22:15:16 -0500

    feat: code refactor to use React Router

--- src/molecules/Article.js
+++ src/molecules/Article.js
@@ -1,19 +1,11 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { ArticlesContext } from "../context/ArticleContext";
 
 import Image from "../atoms/Image";
 
 const Article = ({ article }) => {
-	// eslint-disable-next-line
-	const [selectedArticle, setSelectedArticle] = useContext(ArticlesContext);
-
 	return (
-		<Link
-			to={`/articles/${article.article_id}`}
-			className="w-full h-full mb-3"
-			onClick={() => setSelectedArticle(article.article_id)}
-		>
+		<Link to={`/articles/${article.article_id}`} className="w-full h-full mb-3">
 			<Image src={article.img_url} alt="article banner" />
 			<h1 className="bg-gray-800 text-md font-text py-7 px-5">{article.title}</h1>
 		</Link>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,32 +1,29 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import { ArticlesContext } from "../context/ArticleContext";
 import { getArticle } from "../helpers/articlesCRUD";
 
 const ArticlePage = () => {
 	// eslint-disable-next-line
-	const [articleData, setArticleData] = useState(null);
-	// eslint-disable-next-line
-	const [selectedArticle, setSelectedArticle] = useContext(ArticlesContext);
+	const [articleData, setArticleData] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
-		console.log(id, "Peter here");
+		console.log(id);
 	}, [id]);
 
 	useEffect(() => {
-		if (selectedArticle) {
+		if (id) {
 			async function fetchData() {
-				const data = await getArticle(selectedArticle);
+				const data = await getArticle(id);
 				setArticleData(data);
 			}
 			fetchData();
 		}
 		return () => {};
-	}, [selectedArticle]);
+	}, [id]);
 
-	return <div className="mt-32">{articleData?.title}</div>;
+	return <div></div>;
 };
 
 export default ArticlePage;
